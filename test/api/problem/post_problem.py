9056160dcf02419c772b3272a4f8086a5e6154dd
Tocknicsu <wingemerald@gmail.com>
2016-06-30 15:17:31 +0800

    add more problem permission

---
+++ test/api/problem/post_problem.py
@@ -0,0 +1,86 @@
+data = [
+    {
+        "name": "test_post_problem_no_login",
+        "url": "/api/problems/",
+        "method": "post",
+        "payload": {
+            "title": "problem A",
+            "score_type": 0
+        },
+        "files": {
+            "pdf": "./api/problem/problem.pdf"
+        },
+        "response_status": 403,
+        "response_data": {
+            "msg": "Permission Denied"
+        }
+    }, 
+    {
+        "name": "test_post_problem_admin",
+        "url": "/api/problems/",
+        "method": "post",
+        "payload": {
+            "title": "problem A",
+            "score_type": 0,
+            "token": "ADMIN@TOKEN",
+        },
+        "files": {
+            "pdf": "./api/problem/problem.pdf"
+        },
+        "response_status": 200,
+        "response_data": {
+            "msg": {"id": 1, "score_type": 0, "title": "problem A", "testdata": [], "executes": []}
+        }
+    }, 
+    {
+        "name": "test_post_problem_test",
+        "url": "/api/problems/",
+        "method": "post",
+        "payload": {
+            "title": "problem A",
+            "score_type": 0,
+            "token": "TEST@TOKEN",
+        },
+        "files": {
+            "pdf": "./api/problem/problem.pdf"
+        },
+        "response_status": 403,
+        "response_data": {
+            "msg": "Permission Denied"
+        }
+    }, 
+    {
+        "name": "test_post_problem_unofficial",
+        "url": "/api/problems/",
+        "method": "post",
+        "payload": {
+            "title": "problem A",
+            "score_type": 0,
+            "token": "UNOFFICIAL@TOKEN",
+        },
+        "files": {
+            "pdf": "./api/problem/problem.pdf"
+        },
+        "response_status": 403,
+        "response_data": {
+            "msg": "Permission Denied"
+        }
+    }, 
+    {
+        "name": "test_post_problem_official",
+        "url": "/api/problems/",
+        "method": "post",
+        "payload": {
+            "title": "problem A",
+            "score_type": 0,
+            "token": "OFFICIAL@TOKEN",
+        },
+        "files": {
+            "pdf": "./api/problem/problem.pdf"
+        },
+        "response_status": 403,
+        "response_data": {
+            "msg": "Permission Denied"
+        }
+    }, 
+]
---
+++ test/api/problem/put_problem.py
@@ -0,0 +1,103 @@
+data = [
+    {
+        "name": "test_post_problem_admin",
+        "url": "/api/problems/",
+        "method": "post",
+        "payload": {
+            "title": "problem A",
+            "score_type": 0,
+            "token": "ADMIN@TOKEN",
+        },
+        "files": {
+            "pdf": "./api/problem/problem.pdf"
+        },
+        "response_status": 200,
+        "response_data": {
+            "msg": {"id": 1, "score_type": 0, "title": "problem A", "testdata": [], "executes": []}
+        }
+    }, 
+    {
+        "name": "test_put_problem_no_login",
+        "url": "/api/problems/1/",
+        "method": "put",
+        "payload": {
+            "title": "problem A",
+            "score_type": 0
+        },
+        "files": {
+            "pdf": "./api/problem/problem.pdf"
+        },
+        "response_status": 403,
+        "response_data": {
+            "msg": "Permission Denied"
+        }
+    }, 
+    {
+        "name": "test_put_problem_admin",
+        "url": "/api/problems/1/",
+        "method": "put",
+        "payload": {
+            "title": "problem A",
+            "score_type": 1,
+            "token": "ADMIN@TOKEN"
+        },
+        "files": {
+            "pdf": "./api/problem/problem.pdf"
+        },
+        "response_status": 200,
+        "response_data": {
+            "msg": {"id": 1, "score_type": 1, "title": "problem A", "testdata": [], "executes": []}
+        }
+    }, 
+    {
+        "name": "test_put_problem_test",
+        "url": "/api/problems/1/",
+        "method": "put",
+        "payload": {
+            "title": "problem A",
+            "score_type": 1,
+            "token": "TEST@TOKEN"
+        },
+        "files": {
+            "pdf": "./api/problem/problem.pdf"
+        },
+        "response_status": 403,
+        "response_data": {
+            "msg": "Permission Denied"
+        }
+    }, 
+    {
+        "name": "test_put_problem_unofficical",
+        "url": "/api/problems/1/",
+        "method": "put",
+        "payload": {
+            "title": "problem A",
+            "score_type": 1,
+            "token": "UNOFFICIAL@TOKEN"
+        },
+        "files": {
+            "pdf": "./api/problem/problem.pdf"
+        },
+        "response_status": 403,
+        "response_data": {
+            "msg": "Permission Denied"
+        }
+    }, 
+    {
+        "name": "test_put_problem_officical",
+        "url": "/api/problems/1/",
+        "method": "put",
+        "payload": {
+            "title": "problem A",
+            "score_type": 1,
+            "token": "OFFICIAL@TOKEN"
+        },
+        "files": {
+            "pdf": "./api/problem/problem.pdf"
+        },
+        "response_status": 403,
+        "response_data": {
+            "msg": "Permission Denied"
+        }
+    }, 
+]
