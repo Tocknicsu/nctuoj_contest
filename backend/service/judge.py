75d356188ece082dccd0f87436da38462fb1f22c
allenwhale <allenwhale@yahoo.com.tw>
2016-07-06 14:48:06 +0800

    fixed service/judge bug

--- backend/service/judge.py
+++ backend/service/judge.py
@@ -41,17 +41,29 @@ class Judge(BaseService):
         return (None, None)
 
     def post_submission(self, data):
-        required_args [{
+        required_args = [{
             'name': '+submission_id',
             'type': int,
         }]
         err = self.form_validation(data, required_args)
         if err: return (err, None)
+        err, submission = yield from Service.Submission.get_submission({'id': data['submission_id']})
+        err, problem = yield from Service.Problem.get_problem({'id': submission['problem_id']})
         submission_testdata = (yield self.db.execute("SELECT * FROM map_submission_testdata WHERE submission_id=%s", (data['submission_id'],))).fetchall()
-        time = 0
-        memory = 0
-        verdict = 10
-        for x in submission_testdata:
-            if x['time_usage'] is not None: time = max(time, x['time_usage'])
-            if x['memory_usage'] is not None: memory = max(memory, x['memory_usage'])
+        data = {}
+        data['time_usage'] = max((x['time_usage'] for x in submission_testdata if x['time_usage'] is not None), default=0)
+        data['memory_usage'] = max((x['memory_usage'] for x in submission_testdata if x['memory_usage'] is not None), default=0)
+        data['verdict_id'] = min(x['verdict_id'] for x in submission_testdata)
+        if problem['score_type'] == 0: #sum
+            data['score'] = sum(x['score'] for x in submission_testdata)
+        elif problem['score_type'] == 1: #min
+            data['score'] = min(x['score'] for x in submission_testdata)
+        else:
+            data['score'] = 0
+        self.log(data)
+        sql, param = self.gen_update_sql('submissions', data)
+        yield self.db.execute(sql + ' WHERE id = %s;', param + (submission['id'],))
+        return (None, None)
+
+        
 
