171ce984d8f32b64a6689c03f95b6cf5915b5148
Tocknicsu <wingemerald@gmail.com>
2016-07-09 12:13:33 +0800

    fixed typo

--- backend/service/base.py
+++ backend/service/base.py
@@ -15,9 +15,9 @@ class BaseService:
                 del data[col]
         sql1 = ''.join( ' "%s",'%col for col in data )[:-1]
         sql2 = (' %s,'*len(data))[:-1]
-        prama = tuple( val for val in data.values() )
+        param = tuple( val for val in data.values() )
         sql = 'INSERT INTO "%s" (%s) VALUES(%s) RETURNING id;' % (tablename, sql1, sql2)
-        return (sql, prama)
+        return (sql, param)
     
     def gen_update_sql(self, tablename, _data):
         data = copy(_data)
@@ -25,9 +25,9 @@ class BaseService:
             if _data[col] is None:
                 del data[col]
         sql = ''.join(' "%s" = %%s,'%col for col in data)[:-1]
-        prama = tuple( val for val in data.values() )
+        param = tuple( val for val in data.values() )
         sql = 'UPDATE "%s" SET %s '%(tablename, sql)
-        return (sql, prama)
+        return (sql, param)
 
     def gen_select_sql(self, tablename, data):
         sql = ''.join(' "%s",'%col for col in data)[:-1]
