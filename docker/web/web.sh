eb1f531733e32d80d6df19aa15ca200477988dbe
allenwhale <allenwhale@yahoo.com.tw>
2016-07-23 15:51:47 +0800

    update web docker

--- docker/web/web.sh
+++ docker/web/web.sh
@@ -6,8 +6,7 @@ if [ ! -e "/nctuoj_contest_web" ]; then
     nvm install v6.3.0
     git clone https://github.com/allenwhale/nctuoj_contest_web.git
     cd /nctuoj_contest_web
-    awk '{new=$0; print old; old=new}END{print "Config.baseUrl = \"'"$BASE_URL"'\""; print old}' src/js/utils/Config.js > /tmp/Config.js
-    cp /tmp/Config.js src/js/utils/Config.js
+    awk '{new=$0; print old; old=new}END{print "Config.baseUrl = \"'"$BASE_URL"'\""; print old}' src/js/utils/Config.js.sample > src/js/utils/Config.js
 else
     cd /nctuoj_contest_web
     git pull --rebase
