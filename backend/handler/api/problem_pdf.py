948077a67a16c2ea23b78b7453ec2dd8d7bfe68b
allenwhale <allenwhale@yahoo.com.tw>
2016-07-24 14:28:46 +0800

    update problem pdf handler

--- backend/handler/api/problem_pdf.py
+++ backend/handler/api/problem_pdf.py
@@ -5,6 +5,7 @@ import tornado.gen
 class ProblemPdf(StaticFileHandler):
     @tornado.gen.coroutine
     def get(self, path, include_body=True):
-        path += '/pdf.pdf'
+        # path += '/pdf.pdf'
+        path = chr(ord('A') + int(path) - 1) + '.pdf'
         yield super().get(path, include_body)
 
