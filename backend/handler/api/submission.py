405431cab91127c6e35c9a011fef1a04ba352534
Tocknicsu <wingemerald@gmail.com>
2016-07-28 20:05:30 +0800

    fixed bug with submission verdict

--- backend/handler/api/submission.py
+++ backend/handler/api/submission.py
@@ -50,7 +50,7 @@ class Submission(ApiRequestHandler):
             self.render(err)
         if self.account['isADMIN']:
             err, res['testdata'] = yield from Service.Submission.get_submission_testdata({'id' :id})
-        if res['verdict'] == 4:
+        if res['verdict_id'] == 4:
             err, res['ce'] = (yield from Service.Submission.get_submission_testdata({'id': id}))[0]['note']
         self.render(res)
 
